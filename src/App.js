6c757b807b5f8b6009973931354bb9b4ff79981e
Nevshune <kangdon03@gmail.com>
2023-05-15 14:09:09 +0900

    사용자 변경

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
+import { GlobalStyles } from "./styles";
+import Header from "./Header";
+import MainImage from "./MainImage";
+
 function App() {
   return (
-    <div className="App">
-     <h1>헤더입니다</h1>
-     <section>
-     <div>항목1</div>
-     <div>항목2</div>
-     <div>항목3</div>
-     <div>항목4</div>
-     <div>항목5</div>
-     </section>
-    </div>
+    <>
+      {/* css 리셋 */}
+      <GlobalStyles />
+
+      {/* 메뉴 */}
+      <Header />
+
+      {/* 메인이미지 */}
+      <MainImage />
+    </>
   );
 }
 
