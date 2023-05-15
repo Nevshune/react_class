1f1f1dc79aa1423f7e7207ad55a408bb3f8ea7ca
kangelee <asdfg4920@naver.com>
2023-05-15 13:37:34 +0900

    리셋

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>헤더입니다</h1>
+     <section>
+     <div>항목1</div>
+     <div>항목2</div>
+     <div>항목3</div>
+     <div>항목4</div>
+     <div>항목5</div>
+     </section>
     </div>
   );
 }
